aaaf56278f120323f3ffa2768ab74558bc69d6d4
bra0144 <bran0144@umn.edu>
2021-05-10 17:18:33 -0500

    Using switch statements

---
+++ SwitchStatement.groovy
@@ -0,0 +1,12 @@
+def myOpportunity = 5000
+
+switch(myOpportunity) {
+  case 0..999:
+    println "The best approach for opportunities of this size is email."
+    break
+  case 1000..4999:
+    println "The best approach for opportunities of this size is telephone."
+  break
+  default:
+    println "The best approach for opportunities of this size is face to face."
+}

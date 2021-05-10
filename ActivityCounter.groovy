428148cbc415775594f4c796d92eaa7348cb009e
bra0144 <bran0144@umn.edu>
2021-05-10 17:03:45 -0500

    Added a little more code and decrementing

--- ActivityCounter.groovy
+++ ActivityCounter.groovy
@@ -3,3 +3,9 @@ def completedActivities = 0
 completedActivities = completedActivities + 5
 
 println "You have completed ${completedActivities +1} acttivities today."
+
+def totalActitivites = 10
+
+totalActitivites -= completedActivities
+
+println "You now have $totalActitivites acttivities remaining."

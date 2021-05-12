7a55b3db68df5bb5694134b844969f81089c9be7
bra0144 <bran0144@umn.edu>
2021-05-12 09:43:21 -0500

    Accessing attributes and pulling children from classes

--- src/ImportGpsData.groovy
+++ src/ImportGpsData.groovy
@@ -7,3 +7,12 @@ def gpx = slurper.parse(file)
 println gpx.name
 println ''
 println gpx.desc
+println ''
+println gpx.@version
+println gpx.@creator
+
+for (point in gpx.rte.rtept) {
+  println point.@Lat
+  println point.@Lon
+  println point.time
+}

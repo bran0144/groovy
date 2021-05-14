2cbf375bb82f8aeb4e33e4a464131b7b4545351a
bra0144 <bran0144@umn.edu>
2021-05-14 12:53:33 -0500

    Creating a test suite to prevent duplicated code at setup

--- BankAccountTests.groovy
+++ BankAccountTests.groovy
@@ -1,14 +1,20 @@
 import groovy.util.GroovyTestCase
 
 class BankAccountTests extends GroovyTestCase {
+
+  private account
+  def void setUp(){
+    account = new BankAccount(10)
+  }
+  def void tearDown(){
+    account = null
+  }
   def void testCanDepositMoney() {
-    def account = new BankAccount(10)
     account.deposit(5)
     assert 15 == account.balance
   }
   def void testCanWithdrawMoney() {
-    def account = new BankAccount(15)
     account.withdrawl(5)
-    assert 10 == account.balance
+    assert 5 == account.balance
   }
 }

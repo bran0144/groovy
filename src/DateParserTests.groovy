22200505f4244be95cd55a1e5e199deaddde7c20
bra0144 <bran0144@umn.edu>
2021-05-14 15:57:34 -0500

    Got rid of suplication

--- src/DateParserTests.groovy
+++ src/DateParserTests.groovy
@@ -5,16 +5,18 @@ import org.joda.time.DateTime
 import groovy.util.GroovyTestCase
 
 class DateParserTest extends GroovyTestCase {
-  def void testCanParseDateTime() {
-    def parser = new DateParser()
+  private DateParser parser
 
+  def void setUp(){
+    parser = new DateParser()
+  }
+  def void testCanParseDateTime() {
     def dateTime = new DateTime(2013,1,1,9,30)
     def result = parser.parse(dateTime.toString())
 
     assert '01/01/2013 - 09:30 AM' == result
   }
   def void testWillThrowAnErrorWhenNullDateTimeIsProvided() {
-    def parser = new DateParser()
     shouldFail(IllegalArgumentException) {
       parser.parse(null)
     }
